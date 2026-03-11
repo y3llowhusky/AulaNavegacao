9a0e41c920ac3ebf213f7217d6187fb8ca82c38a
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-11 10:38:39 -0300

    feat: added tab navigator icons

--- src/routes/tab.routes.js
+++ src/routes/tab.routes.js
@@ -2,6 +2,7 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Home from "../screens/Home";
 import Login from "../screens/Login";
 import Service from "../screens/Service";
+import { Feather, MaterialCommunityIcons, MaterialIcons } from "@expo/vector-icons";
 
 // Criando a constante Tab para configurar nossa navegação
 const Tab = createBottomTabNavigator();
@@ -9,9 +10,14 @@ const Tab = createBottomTabNavigator();
 export default function TabRoutes(){
     return (
         <Tab.Navigator screenOptions={{headerTintColor:"red"}}>
-            <Tab.Screen name="Home" component={Home}/>
-            <Tab.Screen name="Login" component={Login}/>
-            <Tab.Screen name="Service" component={Service}/>
+            <Tab.Screen name="Home" component={Home} 
+            options={{tabBarIcon:()=><Feather name="home" color="red" size={20}/>}}/>
+
+            <Tab.Screen name="Login" component={Login}
+            options={{tabBarIcon:()=><MaterialCommunityIcons name="login" color="red" size={20}/>}}/>
+
+            <Tab.Screen name="Service" component={Service}
+            options={{tabBarIcon:()=><MaterialIcons name="miscellaneous-services" color="red" size={20}/>}}/>
         </Tab.Navigator>
     )
 }
